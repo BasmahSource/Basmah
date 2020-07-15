16bdb186eb4df4a3ae2f2c86bdc39b502c744c0d
basmahsource <68329112+basmahsource@users.noreply.github.com>
2020-07-15 19:37:33 +0300

    Update Run.lua

--- inc/Run.lua
+++ inc/Run.lua
@@ -97,7 +97,7 @@ Cr_file:write(Token)
 Cr_file:close()
 print('\27[1;36m￤Token.txt is created.\27[m')
 local Text = "🙋🏼‍♂️¦ اهلا عزيزي [المطور الاساسي](tg://user?id="..SUDO_USER..") \n🔖¦ شكرا لاستخدامك سورس بصمة \n📡¦ أرســل  الان /start\n📛¦ لاضهار الاوامر للمطور  المجهزه بالكيبورد\n\n⚡️["..u.."]"
-https.request(Api_Token..'/sendMessage?chat_id='..SUDO_USER..'&text='..URL.escape(Text)..'&parse_mode=Markdown')
+https.request(Api_Token..'/sendMessage?chat_id='..SUDO_USER..'&text='..URL.escape(Text)..'&parse_mode=Markdow')
 os.execute([[
 rm -f ./README.md
 rm -rf ./.git
